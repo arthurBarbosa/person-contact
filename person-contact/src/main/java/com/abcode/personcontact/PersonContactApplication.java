3cb474a2e1d093eb96d8a0c846673a2b959cc70f
Arthur Barbosa <arthurbruno03@gmail.com>
2021-01-02 20:56:31 -0400

    created project backend

---
+++ person-contact/src/main/java/com/abcode/personcontact/PersonContactApplication.java
@@ -0,0 +1,13 @@
+package com.abcode.personcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonContactApplication.class, args);
+	}
+
+}
